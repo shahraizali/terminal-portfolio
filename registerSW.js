53ffe92390ae988d6bf3fc486f051a782aed97c4
shahraiz <shahraizali10@yahoo.com>
2023-01-22 04:19:38 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/terminal-portfolio/sw.js', { scope: '/terminal-portfolio/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
