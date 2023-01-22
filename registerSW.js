d18a424f32fe7bd981a5a1206b63351dfc6d2082
shahraiz <shahraizali10@yahoo.com>
2023-01-22 04:07:51 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/%3Crepo%3E/sw.js', { scope: '/%3Crepo%3E/' })})}
